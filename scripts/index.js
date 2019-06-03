8337641cf874e90d0fca6c8a9a45357d1ca05f8f
Dave Caruso <dave@davecode.me>
2019-06-03 07:36:38 -0400

    add --version argument

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 const cli = require('cli');
 
+cli.setApp('fullstack-system', require('../package.json').version)
+cli.enable('version');
+
 cli.parse(null, ['dev', 'build', 'new', 'production']);
 
 require('./' + cli.command);
